5a1b183133b4d36f471d7a2474b892d051a22fd3
Abhishek Rathore <abhishekrathore468@gmail.com>
2025-02-05 15:25:54 +0530

    first commit

---
+++ lib/clerk.ts
@@ -0,0 +1,6 @@
+// import  {Clerk}  from "@clerk/clerk-sdk-node";
+
+// // Initialize the Clerk client
+// const clerkClient = new Clerk({ apiKey: process.env.CLERK_SECRET_KEY });
+
+// export { clerkClient };
