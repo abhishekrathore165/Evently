386d97d2edd300dd505e6b8db52e146ac316c16b
Abhishek Rathore <abhishekrathore468@gmail.com>
2025-02-20 16:20:21 +0530

    firstcommit

--- types/index.ts
+++ types/index.ts
@@ -145,5 +145,5 @@ export type CreateUserParams = {
   
   export type SearchParamProps = {
     params: Promise<{ id: string }>
-    searchParams: { [key: string]: string | string[] | undefined }
+    // searchParams: { [key: string]: string | string[] | undefined }
   }
